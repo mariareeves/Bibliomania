d7f5f762c06b7c4173b4f6693e2a1bfe5d35f17a
mariareeves <mariapaula.zimmers@gmail.com>
2023-01-24 09:50:48 -0700

    create a new page for review

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -5,17 +5,17 @@ module.exports = {
     create,
 }
 
-function newReview(req, res) {
-    res.render('reviews/new', { title: 'Add a Review' });
-}
 
 function create(req, res) {
     console.log('Testing controller')
-    Book.findById(req.params.id, function (err, book) {
-        console.log(err)
-        book.reviews.push(req.body)
-        book.save(function (err) {
-            res.redirect(`/books/${book._id}`)
-        })
+    bookId = req.params.id
+    req.body.book = bookId
+    Book.create(req.body, function (err, review) {
+        res.redirect(`/books/${bookId}`)
     })
 }
+
+function newReview(req, res) {
+    res.render('reviews/new', { title: 'Add Review', bookId: req.params.id });
+}
+
--- routes/review.js
+++ routes/review.js
@@ -4,7 +4,7 @@ const reviewsCtrl = require('../controllers/reviews');
 
 // New form for a review
 // GET /books/:id/reviews/new
-router.get('/:id/reviews/new', reviewsCtrl.new)
+router.get('/books/:id/reviews/new', reviewsCtrl.new)
 // Post /create/:id/reviews
 router.post('/books/:id/reviews', reviewsCtrl.create)
 
