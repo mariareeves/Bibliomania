c11e4ca168df0fb5aabb55b0994be8530ac45e97
mariareeves <mariapaula.zimmers@gmail.com>
2023-01-24 11:38:16 -0700

    user can add a review to a book

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -6,13 +6,36 @@ module.exports = {
 }
 
 
+// function create(req, res) {
+
+//     console.log('Testing controller')
+//     console.log(req.body)
+//     console.log(req.body.content)
+//     bookId = req.params.id
+//     req.body.book = bookId
+//     Book.create(req.body, function (err, review) {
+//         res.redirect(`/books/${bookId}`)
+//     })
+// }
+
 function create(req, res) {
-    console.log('Testing controller')
-    bookId = req.params.id
-    req.body.book = bookId
-    Book.create(req.body, function (err, review) {
-        res.redirect(`/books/${bookId}`)
-    })
+    Book.findById(req.params.id, function (err, book) {
+        if (err) {
+            console.log("There's an error", err);
+            res.redirect("/books");
+        } else {
+            console.log(req.user._id)
+            req.body.user = req.user._id
+            req.body.userName = req.user.name
+            req.body.userAvatar = req.user.avatar
+
+            book.reviews.push(req.body)
+            book.save(function (err) {
+                console.log(book.reviews[0].content)
+                res.redirect(`/books/${req.params.id}`)
+            });
+        }
+    });
 }
 
 function newReview(req, res) {
